b890abdababf2bd2e51b5d4110354ce01119fab7
darkCavalier11 <sumit.kumar@changepay.in>
2024-10-15 13:10:11 +0530

    Added fetch and save for payload

--- Sources/YoutubeInterface/Models/UserInternalData+CoreDataClass.swift
+++ Sources/YoutubeInterface/Models/UserInternalData+CoreDataClass.swift
@@ -8,8 +8,33 @@
 
 import Foundation
 import CoreData
+import os
 
 @objc(UserInternalData)
 public class UserInternalData: NSManagedObject {
-
+  private lazy var coreDataStack: CoreDataStack = {
+    return CoreDataStack(modelName: "MusicSession")
+  }()
+  
+  private let logger = Logger(subsystem: "com.youtube.interface", category: "CoreData/UserInternalData")
+  
+  func getLatestUserRequestPayload() -> Data? {
+    let request = UserInternalData.fetchRequest()
+    let results = try? coreDataStack.managedObjectContext.fetch(request)
+    
+    guard results != nil, results!.count > 0 else {
+      logger.debug("Request payload not found in db")
+      return nil
+    }
+    
+    logger.debug("Found request payload in db")
+    return results?.first?.payload
+  }
+  
+  func saveLatestUserRequestPayload(_ payload: Data) {
+    let userInternalData = UserInternalData(context: coreDataStack.managedObjectContext)
+    userInternalData.payload = payload
+    logger.debug("Saving user internal data payload")
+    try? coreDataStack.managedObjectContext.save()
+  }
 }
